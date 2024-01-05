8c5c3e640df0f62fa4f8b5f7fcc272a5715d7002
aldojimenezw <aldojimenezw@gmail.com>
2024-01-04 21:33:24 -0300

    installing 3 pluggins

---
+++ lua/plug/fugitive.lua
@@ -0,0 +1,6 @@
+return {
+	"tpope/vim-fugitive",
+	keys = {
+		{"<leader>gs","<cmd>Git<cr>", desc="git status with fugitive"},
+	},
+}
--- lua/plug/telescope.lua
+++ lua/plug/telescope.lua
@@ -1,5 +1,10 @@
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+	    {"<leader>ff", "<cmd>Telescope find_files<cr>", desc = "find files"},
+	    {"<leader>fg", "<cmd>Telescope git_files<cr>", desc = "git files"},
+	    {"<leader>fs", "<cmd>Telescope grep_string<cr>", desc = "find words"},
+    },
+}
